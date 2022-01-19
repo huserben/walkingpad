a39398851d8c6678037594014719a5fd1fff4d87
Nathan Glover <nathan@glovers.id.au>
2022-01-19 23:44:36 +0800

    dota 2 charge bot

---
+++ dota/README.md
@@ -0,0 +1,9 @@
+# Dota Bot
+
+> **NOTE**: Windows Only
+
+```bash
+python -m venv .venv
+source ./.venv/Scripts/activate
+pip3 install -r requirements.txt
+```
---
+++ dota/utils.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv.resize(image, dim, interpolation=inter)
